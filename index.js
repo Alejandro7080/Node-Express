8dc662d500b66782136d32fc2d0d7455e19d3ad9
TXT-39 <txt-39@MacBook-Pro-42.attlocal.net>
2021-06-25 19:11:35 -0700

    First commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express();
+const port = 3002;
+
+app.get('/', function(req, res){
+    res.send("Hello");
+})
+app.get('/home', function(req, res){
+    console.log(__dirname);
+    res.sendFile(__dirname + '/index.html');
+})
+app.get('/about', function(req, res){
+    console.log(__dirname);
+    res.sendFile(__dirname + '/index2.html');
+})
+app.get('/contact', function(req, res){
+    console.log(__dirname);
+    res.sendFile(__dirname + '/index3.html');
+})
+app.post('/home', function (req, res){
+    res.send("post() success!");
+})
+app.listen(port, function(){
+    console.log('Listening at port: ' + port)
+})
